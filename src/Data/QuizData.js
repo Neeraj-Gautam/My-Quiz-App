3cef434351250e8dac2b8a710d2fb4715b5c1baa
Neeraj <neeraj23gautam@gmail.com>
2023-05-21 12:35:09 +0530

    initial comit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,35 @@
+export const QuizData = [
+    {
+        Question: "What is the capital of France?",
+        options:[ "new york", "London", "Paris", "Dublin"],
+        Answer: 3
+    },
+    {
+        Question: "Who is CEO of Tesla?",
+        options:["Jeff Bezos", "Elon Musk", "Bill Gates", "Tony Stark"],
+        Answer: 2
+    },
+    {
+        Question: "The iPhone was created by which company?",
+        options: ["Apple", "Intel", "Amazon", "Microsoft"],
+        Answer: 1
+    },
+    {
+        Question: "How many Harry Potter books are there?",
+        options: ["1", "4", "6", "7"],
+        Answer:4
+    },
+    {
+        Question: "What is the name of Google's new Ai?",
+        options: ["Bard AI", "Jasper Chat", "ChatGPT", "Bing AI"],
+        Answer:1
+    }
+];
+
+
+
+
+
+
+
+
